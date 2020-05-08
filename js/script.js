e5afb01e33c1ce8023c02d92929f511dd2ba4964
Adam2 <43184258+adamaka47@users.noreply.github.com>
2020-05-08 16:31:50 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$('.header__slider').slick({
+		arrows: false,
+		vertical: true,
+		verticalSwiping: true,
+		dots: true,
+		autoplay: true,
+		autoplaySpeed: 2500,
+		
+	});
+});
